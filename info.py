88ef5d0d032f092c92876771491574fb35fc6765
searchingshiv <108224889+searchingshiv@users.noreply.github.com>
2023-09-21 22:01:36 +0530

    Update info.py

--- info.py
+++ info.py
@@ -47,7 +47,7 @@ NO_RESULTS_MSG = bool(environ.get("NO_RESULTS_MSG", False))
 # MongoDB information
 DATABASE_URI = environ.get('DATABASE_URI', "")
 DATABASE_NAME = environ.get('DATABASE_NAME', "")
-COLLECTION_NAME = environ.get('COLLECTION_NAME', 'AutoFilter')
+COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files')
 
 # Others
 VERIFY = bool(environ.get('VERIFY', False))
