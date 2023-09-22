be62e2e40cb0d549d287174e6c616d655cab628b
searchingshiv <108224889+searchingshiv@users.noreply.github.com>
2023-09-22 11:50:12 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/Rohaniscoder/EasyAdvAutoFilter.git /EasyAdvAutoFilter
+  git clone https://github.com/searchingshiv/mirrorbot.git /mirrorbot
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /spidyauto
+  git clone $UPSTREAM_REPO /mirrorbot
 fi
-cd /spidyauto
+cd /mirrorbot
 pip3 install -U -r requirements.txt
 echo "Starting Bot...."
 python3 bot.py
